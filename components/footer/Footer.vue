2aa73c9b4d66c34541a6316752c160446aa0ff5d
maidacencoanastasia <maidacenco100@gmail.com>
2021-05-20 09:55:19 +0300

    Initial commit

---
+++ components/footer/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="footer">
+    <div class="columns container">
+      <div class="column">
+        <p>maidacenco100@gmail.com</p>
+      </div>
+    <div class="column has-text-right">
+      <p>Shop | Made with pain&stress&tears</p>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VmFooter',
+}
+</script>
+
+<style lang="scss" scoped>
+ .footer {
+   background: rgba(175, 81, 252, 0.64);
+   color: black;
+ }
+</style>
---
+++ components/hero/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hero is-medium is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Online Shop
+        </h1>
+        <h2 class="subtitle">
+          All what you need
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    .hero {
+      background: rgba(173, 80, 248, 0.64);
+    }
+    h1, h2 {
+      color: #fff;
+      //font-size: 38px;
+    }
+</style>
